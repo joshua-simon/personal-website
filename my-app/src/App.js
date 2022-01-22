eeabb7c8a9d1013b9c79154cc33f40663a708aea
Joshua Simon <joshy.d.simon@gmail.com>
2022-01-22 20:14:06 +1300

    skills section added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Projects from "./components/Projects";
+import Skills from "./components/Skills";
 import './styles.css'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Main />
       <Projects />
+      <Skills/>
     </>
   );
 }
---
+++ my-app/src/components/Skills.js
@@ -0,0 +1,40 @@
+
+const Skills = () => {
+    return(
+        <div className="skills-container">
+            <h1>Skills</h1>
+            <div className="skills-grid">
+                <div className="skills-grid-item">
+                    <p>JavaScript (ES5/ES6)</p>
+                    <img src="https://img.icons8.com/ios/50/ffffff/javascript-logo.png"/>
+                </div>
+                <div className="skills-grid-item">
+                    <p>React</p>
+                    <img  src="https://img.icons8.com/ios-glyphs/30/ffffff/react.png"/>
+                </div>
+                <div className="skills-grid-item">
+                    <p>Firebase</p>
+                    <img src="https://img.icons8.com/color/48/ffffff/firebase.png"/>
+                </div>
+                <div className="skills-grid-item">
+                    <p>Node.js</p>
+                    <img src="https://img.icons8.com/color/48/ffffff/nodejs.png"/>
+                </div>
+                <div className="skills-grid-item">
+                    <p>Git/Github</p>
+                    <img src="https://img.icons8.com/ios-filled/50/ffffff/github.png"/>
+                </div>
+                <div className="skills-grid-item">
+                    <p>SQLite3</p>
+                    <img src="https://img.icons8.com/ios-filled/50/ffffff/database.png"/>
+                </div>
+                <div className="skills-grid-item">
+                    <p>Jest/React Testing Library</p>
+                    <img src="https://img.icons8.com/ios/50/ffffff/thin-test-tube.png"/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Skills
