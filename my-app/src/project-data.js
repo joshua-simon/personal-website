51a00779c909067a50f905862df1d5489aa09621
Joshua Simon <joshy.d.simon@gmail.com>
2022-02-17 20:53:05 +1300

    project added

--- my-app/src/project-data.js
+++ my-app/src/project-data.js
@@ -1,16 +1,17 @@
 import fml from './images/fml2.png'
-import hashi from './images/hashi_image.png'
+import wellyadvisor from './images/wellyadvisor.png'
 import instagram from './images/instagram-ish.png'
-import wcr from './images/wcr_image.png'
+
 
 
 const projects = [
     {
-        name: 'Instagram-ish',
-        url: "https://github.com/joshua-simon/instagram-clone",
-        image: instagram,
-        techStack: 'React, Firebase, Tailwind CSS, React Testing Library',
-        blurb: 'An instagram clone, built to develop my skills with React Context and testing.'
+        name: 'Wellyadvisor',
+        url: 'https://welly-advisor.herokuapp.com/',
+        image:wellyadvisor,
+        techStack: 'React, Firebase, CSS',
+        blurb: 'A review website for Wellington, based on TripAdvisor.',
+        code:'https://github.com/joshua-simon/welly-trip-advisor'
     },
     {
         name: 'Follow My Lead',
@@ -21,20 +22,15 @@ const projects = [
         code: 'https://github.com/kahu-2020/Follow-My-Lead'
     },
     {
-        name: 'The Wellington Coffee Review',
-        url: 'https://welly-coffee-review-8eda1.web.app/',
-        image:wcr,
-        techStack: 'React, Firebase, CSS',
-        blurb: 'A cafe review website for Wellington, loosely based on TripAdvisor.',
-        code:'https://github.com/joshua-simon/welly-coffee-review-v2'
+        name: 'Instagram-ish',
+        url: "https://github.com/joshua-simon/instagram-clone",
+        image: instagram,
+        techStack: 'React, Firebase, Tailwind CSS, React Testing Library',
+        blurb: 'An instagram clone, built to develop my skills with React Context and testing.'
     },
-    {
-        name: 'Hashigo Zake website',
-        url: 'https://hashigo-zake.herokuapp.com/',
-        image:hashi,
-        techStack: 'React,CSS, MailChimp',
-        blurb: "A website for one of Wellington's best craft beer bars"
-    }
+
 ]
+
+
 
 export default projects
